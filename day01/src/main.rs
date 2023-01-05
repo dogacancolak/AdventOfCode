2f1011e8cb3a124d3a8bb65a4bfc3b150d44dc6b
Dogacan Colak <dogacancolak@gmail.com>
2023-01-05 17:57:57 -0500

    day 1

---
+++ day01/src/main.rs
@@ -0,0 +1,63 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::cmp;
+
+fn main() {
+    // part 1
+    most_calories();
+
+    // part 2
+    top_3_most_calories();
+}
+
+// Part 1 main function
+fn most_calories() {
+    let file = File::open("src/input.txt").expect("Cannot open file");
+    let file = BufReader::new(file);
+    let mut curr_elf = 0;
+    let mut max = 0;
+    for line in file.lines().filter_map(|result| result.ok()) {
+        if line.is_empty() {
+            max = cmp::max(max, curr_elf);
+            curr_elf = 0;
+        }
+        else {
+            let calory = line.parse::<i32>().unwrap();
+            curr_elf += calory;
+        }
+    }
+    println!("The max is {}", max);
+}
+
+// Part 2 main function
+fn top_3_most_calories() {
+    let file = File::open("src/input.txt").expect("Cannot open file");
+    let file = BufReader::new(file);
+    let mut curr_elf = 0;
+    let mut first = 0;
+    let mut second = 0;
+    let mut third = 0;
+    for line in file.lines().filter_map(|result| result.ok()) {
+        if line.is_empty() {
+            if curr_elf > first {
+                third = second;
+                second = first;
+                first = curr_elf;
+            }
+            else if curr_elf > second {
+                third = second;
+                second = curr_elf;
+            }
+            else if curr_elf > third {
+                third = curr_elf;
+            }
+            curr_elf = 0;
+        }
+        else {
+            let calory = line.parse::<i32>().unwrap();
+            curr_elf += calory;
+        }
+    }
+
+    println!("The sum of the max 3 is {}", first + second + third);
+}
