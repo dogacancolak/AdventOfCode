08e9db8879ba2011df653a4ca390f5b03177425e
Dogacan Colak <dogacancolak@gmail.com>
2023-01-05 17:55:05 -0500

    day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,6 +8,10 @@ const MY_ROCK: &str = "X";
 const MY_PAPER: &str = "Y";
 const MY_SCISSORS: &str = "Z";
 
+const LOSE: &str = "X";
+const DRAW: &str = "Y";
+const WIN: &str = "Z";
+
 enum PlayMove {
     Rock,
     Paper,
@@ -21,10 +25,15 @@ enum PlayResult {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // Part 1
     calculate_my_score();
+
+    // Part 2
+    calculate_my_score_different_rules();
 }
 
+
+// Part 1 main function
 fn calculate_my_score() {
     let file = File::open("src/input.txt").expect("Cannot open file");
     let file = BufReader::new(file);
@@ -57,15 +66,13 @@ fn parse_move(play_move: &str) -> PlayMove {
 
 fn play_round(opponent_move: &PlayMove, my_move: &PlayMove) -> PlayResult {
     match (opponent_move, my_move) {
-        (PlayMove::Rock, PlayMove::Rock) => PlayResult::Draw,
-        (PlayMove::Paper, PlayMove::Paper) => PlayResult::Draw,
-        (PlayMove::Scissors, PlayMove::Scissors) => PlayResult::Draw,
         (PlayMove::Rock, PlayMove::Paper) => PlayResult::Win,
         (PlayMove::Rock, PlayMove::Scissors) => PlayResult::Lose,
         (PlayMove::Paper, PlayMove::Scissors) => PlayResult::Win,
         (PlayMove::Paper, PlayMove::Rock) => PlayResult::Lose,
         (PlayMove::Scissors, PlayMove::Rock) => PlayResult::Win,
         (PlayMove::Scissors, PlayMove::Paper) => PlayResult::Lose,
+        _ => PlayResult::Draw,
     }
 }
 
@@ -90,4 +97,61 @@ fn evaluate_score(my_move: &PlayMove, result: &PlayResult) -> u32 {
     }
 
     return score;
+}
+
+// Part 2 main
+fn calculate_my_score_different_rules() {
+    let file = File::open("src/input.txt").expect("Cannot open file");
+    let file = BufReader::new(file);
+
+    let mut total_score = 0;
+    for line in file.lines().filter_map(|result| result.ok()) {
+        let row: Vec<&str> = line.split(" ").collect();
+        let opponent_move = parse_move(row[0]);
+        let result = parse_result(row[1]);
+        let my_move = get_move(&opponent_move, &result);
+        total_score += evaluate_score(&my_move, &result);
+    }
+    println!("The total score with different rules is {}", total_score);
+}
+
+fn parse_result(play_move: &str) -> PlayResult {
+    match play_move {
+        LOSE => PlayResult::Lose,
+        DRAW => PlayResult::Draw,
+        WIN => PlayResult::Win,
+        _ => panic!("Unrecognized move {}", play_move)
+    }
+}
+
+fn get_move(opponent_move: &PlayMove, result: &PlayResult) -> PlayMove {
+    match result {
+        PlayResult::Lose => get_move_to_lose(&opponent_move),
+        PlayResult::Draw => get_move_to_draw(&opponent_move),
+        PlayResult::Win => get_move_to_win(&opponent_move),
+    }
+}
+
+fn get_move_to_lose(opponent_move: &PlayMove) -> PlayMove {
+    match opponent_move {
+        PlayMove::Rock => PlayMove::Scissors,
+        PlayMove::Paper => PlayMove::Rock,
+        PlayMove::Scissors => PlayMove::Paper,
+    }
+}
+
+fn get_move_to_draw(opponent_move: &PlayMove) -> PlayMove {
+    match opponent_move {
+        PlayMove::Rock => PlayMove::Rock,
+        PlayMove::Paper => PlayMove::Paper,
+        PlayMove::Scissors => PlayMove::Scissors,
+    }
+}
+
+fn get_move_to_win(opponent_move: &PlayMove) -> PlayMove {
+    match opponent_move {
+        PlayMove::Rock => PlayMove::Paper,
+        PlayMove::Paper => PlayMove::Scissors,
+        PlayMove::Scissors => PlayMove::Rock,
+    }
 }
